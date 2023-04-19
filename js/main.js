eb4f0ae00156bb2c7e270ed1242fca187fe50caf
Omar Marey <omer4162@gmail.com>
2023-04-19 06:29:54 +0200

    mostly finished the personal website

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const handleOnMouseMove = e =>{
+    for (const card of document.getElementsByClassName('card')) {
+
+        
+    
+
+    const rect = card.getBoundingClientRect(),
+    x= e.clientX- rect.left
+    y = e.clientY - rect.top;
+    
+    card.style.setProperty('--mouse-x',`${x}px`)
+    card.style.setProperty('--mouse-y',`${y}px`)
+}
+}
+
+let cards = document.querySelector('.cards')
+cards.onmousemove = e => handleOnMouseMove(e);
