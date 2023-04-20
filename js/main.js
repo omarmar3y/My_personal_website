6e5c4c541ba1acbc1c0a0e8d16e724c2134bf045
Omar Marey <omer4162@gmail.com>
2023-04-20 04:38:01 +0200

    added new style for the projects cards

--- js/main.js
+++ js/main.js
@@ -13,6 +13,31 @@ const handleOnMouseMove = e =>{
     card.style.setProperty('--mouse-y',`${y}px`)
 }
 }
+const handleScrollAction = ()=>{
+    const rect = cards.getBoundingClientRect().top,
+    inHieght = window.innerHeight,
+    clientH = document.documentElement.clientHeight,
+    cardsList = document.querySelectorAll('.card');
+    if (rect <= (inHieght||clientH)){
+        let i = 0;
+        for (const ele of cardsList) {
+            
+            ele.classList.add('scrolled')
+            ele.style.setProperty("--delay-time",`${0.2 *i }s`)
+            i++
+        }
+    } else {
+        for (const ele of cardsList) {
+            ele.classList.remove('scrolled')
+            
+        }
+    }
+    
+}
 
 let cards = document.querySelector('.cards')
 cards.onmousemove = e => handleOnMouseMove(e);
+
+window.addEventListener('scroll', () => {
+    handleScrollAction();
+})
